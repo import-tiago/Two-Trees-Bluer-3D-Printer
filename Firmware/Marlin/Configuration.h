eb08137a6ed5e9d3095c4784de9d6a44b9d6c5f3
Tiago Silva <tiagodepaulasilva@gmail.com>
2020-11-28 11:20:32 -0300

    E0 DEFAULT_MAX_ACCELERATION from 10k to 1k

--- Firmware/Marlin/Configuration.h
+++ Firmware/Marlin/Configuration.h
@@ -769,7 +769,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 1000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
